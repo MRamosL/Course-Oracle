41a1a846874f667cd2fc185dc8cc78bddc15e6e1
MRamosL <1536425@senati.pe>
2024-03-01 07:09:13 -0500

    Archivo1

---
+++ ejercicio1.sql
@@ -0,0 +1,8 @@
+/*
+Ejercicio 01. Listar Todos los customers que inicien su id con la 
+letra 'A'. Guardas el archivo como 'ejercicio01.sql' y lo
+subes a tu repositorio
+*/
+
+select * from Customers 
+where CustomerID like 'A%'
